2d56e3ca00662e5b0fc38dda30d67f5b5730527c
sgonzalez55 <santigogonzalezlozano@gmail.com>
2022-10-10 07:38:04 -0500

    modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-let btnSelect = document.getElementById('select')
+function iniciar() {
+    let btnSelect = document.getElementById('select')
+    btnSelect.addEventListener('click', seleccionarMokepon)
+    function seleccionarMokepon() {
+        alert("TEST")
+    }
+}
+
+
+window.addEventListener('load', iniciar)
